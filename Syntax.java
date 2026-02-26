e124c1a48b0db3eff526f1db9201bc9c1265fdcf
Krishnakant Tiwari <122863169+krishnakanttiwariSecU32@users.noreply.github.com>
2026-02-26 22:52:17 +0530

    Initial commit

--- Syntax.java
+++ Syntax.java
@@ -1,5 +1,13 @@
 class Syntax{
     public static void main(String[] args) {
-        System.out.println("kris");
+        int number = 10
+        if(number > 5) {
+            System.out.println("Number is greater than 5")
+        else {
+            System.out.println("Number is less than or equal to 5");
+        }
     }
 }
+
+// So here are three syntax errors that are semicolon after the number 10 in the line no. 3 and also in the line 5 and not nat have the closing braces for the class Syntax
+
